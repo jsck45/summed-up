bb8c5a8d25b8b1197947c9569dddcdb85f3d50c1
thorsten <90459268+ThorstenBeowulf@users.noreply.github.com>
2023-10-23 20:49:09 +1100

    fixed typo

--- server/models/index.js
+++ server/models/index.js
@@ -1,5 +1,5 @@
 const Categories = require('./Category');
-const Comment = require('. Comment');
+const Comment = require('./Comment');
 const Post = require('./Post');
 const User = require('./User')
 

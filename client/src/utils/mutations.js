1882204439362d102efd05d9d29a6b7b844ab403
thorsten <90459268+ThorstenBeowulf@users.noreply.github.com>
2023-10-26 20:35:36 +1100

    renamed graphql references to their corresponding query schema

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -13,7 +13,7 @@ export const CREATE_USER = gql`
 `;
 
 export const LOGIN_USER_EMAIL = gql `
-    mutation login($email: String!, $password: String!)
+    mutation loginEmail($email: String!, $password: String!)
     {
         login(email: $email, password: $password) {
             token
@@ -26,7 +26,7 @@ export const LOGIN_USER_EMAIL = gql `
 `;
 
 export const LOGIN_USER_USERNAME = gql `
-    mutation login($username: String!, $password: String!)
+    mutation loginUsername($username: String!, $password: String!)
     {
         login(username: $username, password: $password) {
             token

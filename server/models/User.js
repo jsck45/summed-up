4d5606c8483a03e07a350d5806b3b760804def5b
thorsten <90459268+ThorstenBeowulf@users.noreply.github.com>
2023-10-31 14:31:44 +1100

    fixed user model

--- server/models/User.js
+++ server/models/User.js
@@ -34,7 +34,8 @@ userSchema.pre('save', async function (next) {
 });
 
 userSchema.methods.isCorrectPassword = async function (password) {
-  return bcrypt.compare(password, this.password);
+  const result = await bcrypt.compare(password, this.password);
+  return result;
 };
 
 const User = model('User', userSchema);
